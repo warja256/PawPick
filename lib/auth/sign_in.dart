19df510532e804c62814ab1f325cc7cd21e5325d
Alx1307 <ekhmara75@yandex.ru>
2024-12-07 22:17:56 +0300

    Сделан экран авторизации без валидации полей

--- lib/auth/sign_in.dart
+++ lib/auth/sign_in.dart
@@ -1,15 +1,213 @@
 import 'package:flutter/material.dart';
+import '../registration/registration_1.dart';
+import 'package:paw_pick/filters/filters.dart';
+
+//добавить проверку полей
+//добавить валидацию неправильных данных
 
 class AuthScreen extends StatelessWidget {
   const AuthScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Войдите в свой аккаунт',
-          style: TextStyle(fontSize: 24),
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 70),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                alignment: Alignment.centerLeft,
+                child: Text(
+                    "PAWPICK",
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      height: 1.5,
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                    )
+                  ),            
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 0),
+                alignment: Alignment.centerLeft,
+                child: const Text(
+                    "Войдите в свой аккаунт",
+                    style: TextStyle(
+                      color: Color(0xFF000000),
+                      height: 1.5,
+                      fontSize: 34.0,
+                      fontWeight: FontWeight.bold,
+                    )
+                  ),            
+              ),
+              const SizedBox(height: 70),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Телефон или почта',
+                      style: TextStyle(
+                        fontSize: 12.0,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Container(
+                      width: 335,
+                      height: 56,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: '',
+                          contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                            borderSide: const BorderSide(color: Colors.grey),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                            borderSide: const BorderSide(color: Colors.grey),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                            borderSide: BorderSide(color: Theme.of(context).primaryColor),
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
+                        ),
+                        keyboardType: TextInputType.text,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Пароль',
+                      style: TextStyle(
+                        fontSize: 12.0,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Container(
+                      width: 335,
+                      height: 56,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: '',
+                          contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                            borderSide: const BorderSide(color: Colors.grey),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                            borderSide: const BorderSide(color: Colors.grey),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                            borderSide: BorderSide(color: Theme.of(context).primaryColor),
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
+                        ),
+                        keyboardType: TextInputType.text,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 5),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 40),
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const RegistrationScreen()),//переход на страницу восстановления
+                      );
+                    },
+                    child: Text(
+                      "Забыли пароль?",
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),           
+              ),
+              const SizedBox(height: 5),
+              Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const Filters(),
+                        ),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Theme.of(context).primaryColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      minimumSize: const Size(double.infinity, 56),
+                    ),
+                    child: const Text(
+                      "Войти",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        height: 1.5,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 200),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 110, vertical: 20),
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const RegistrationScreen()),
+                      );
+                    },
+                    child: Text.rich(
+                      TextSpan(
+                        children: [
+                          TextSpan(
+                            text: "Нет аккаунта? ",
+                            style: TextStyle(
+                                color:
+                                    Theme.of(context).textTheme.labelMedium?.color),
+                          ),
+                          TextSpan(
+                            text: "Создать",
+                            style: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+            ]
+          ),
         ),
       ),
     );
